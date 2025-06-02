b63cf4ba6c15d28eaf4d9b4214bbd5352381b795
Asaiy420 <kcaryan05@gmail.com>
2025-06-02 18:23:59 +0545

    add: (feat) auth routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,22 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./lib/DB.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
 const PORT = process.env.PORT;
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/auth", authRoutes);
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
 
 
