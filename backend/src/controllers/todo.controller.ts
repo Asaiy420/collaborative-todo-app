32a11dc4284c64f2251c6aa64f953dd93ad16630
Asaiy420 <kcaryan05@gmail.com>
2025-06-05 19:53:36 +0545

    add: rest features

--- backend/src/controllers/todo.controller.ts
+++ backend/src/controllers/todo.controller.ts
@@ -31,6 +31,68 @@ export const createTodo = async (
 };
 
 export const getTodos = async (
-  req: Request,
+  req: AuthRequest,
   res: Response
-): Promise<void> => {};
+): Promise<void> => {
+  try {
+    const todos = await Todo.find({ owner: req.user?.userId });
+    res.status(200).json(todos);
+    return;
+  } catch (error) {
+    console.error("Error when getting todos:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateTodo = async (
+  req: AuthRequest,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const { title, description, dueDate, collaborators } = req.body;
+
+    if (!id) {
+      res.status(400).json({ message: "Missing id" });
+      return;
+    }
+
+    if (!title || !description || !dueDate || !collaborators) {
+      res.status(400).json({ message: "Missing required fields" });
+      return;
+    }
+
+    const todo = await Todo.findByIdAndUpdate(id, {
+      title,
+      description,
+      dueDate,
+      collaborators,
+    });
+
+    res.status(200).json(todo);
+  } catch (error) {
+    console.error("Error when updating todo:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteTodo = async (
+  req: AuthRequest,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      res.status(404).json({ message: "Todo not found" });
+      return;
+    }
+
+    const todo = await Todo.findByIdAndDelete(id);
+
+    res.status(200).json(todo);
+  } catch (error) {
+    console.error("Error when deleting todo:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/src/routes/todo.route.ts
+++ backend/src/routes/todo.route.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.js";
-import { createTodo } from "../controllers/todo.controller.js";
+import { createTodo, getTodos, updateTodo, deleteTodo } from "../controllers/todo.controller.js";
 
 const router = Router();
 
+router.get("/", protectRoute, getTodos)
 router.post("/create", protectRoute, createTodo);
+router.put("/update/:id", protectRoute, updateTodo);
+router.delete("/delete/:id", protectRoute, deleteTodo);
 
 export default router;
