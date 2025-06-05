96ef254f7d41a5fc1c2c8c530e3c3a0d242a61f7
Asaiy420 <kcaryan05@gmail.com>
2025-06-05 19:53:19 +0545

    fix : linting issue

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -10,7 +10,7 @@ export interface AuthRequest extends Request {
 }
 
 export const protectRoute = async (
-  req: AuthRequest,
+  req: AuthRequest, 
   res: Response,
   next: NextFunction
 ): Promise<void> => {
