b8e8473aa8bac2da41670d589760f2965fc1ac25
Asaiy420 <kcaryan05@gmail.com>
2025-06-05 16:23:08 +0545

    add: (feat) create todos

--- backend/src/controllers/todo.controller.ts
+++ backend/src/controllers/todo.controller.ts
@@ -1,10 +1,36 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
+import Todo from "../models/todo.model.js";
+import { AuthRequest } from "../middleware/auth.js";
 
+export const createTodo = async (
+  req: AuthRequest,
+  res: Response
+): Promise<void> => {
+  try {
+    const { title, description, dueDate, collaborators } = req.body;
 
-export const createTodo = async (req:Request, res:Response): Promise<void> => {
-    res.send("Hello world")
-}
+    if (!title || !description || !dueDate || !collaborators) {
+      res.status(400).json({ message: "Missing required fields" });
+      return;
+    }
 
-export const getTodos = async (req:Request, res:Response): Promise<void> => {
-    
-}
+    const todo = await Todo.create({
+      title,
+      description,
+      dueDate,
+      collaborators,
+      owner: req.user?.userId,
+    });
+
+    res.status(201).json(todo);
+    return;
+  } catch (error) {
+    console.error("Error when creating todo:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getTodos = async (
+  req: Request,
+  res: Response
+): Promise<void> => {};
